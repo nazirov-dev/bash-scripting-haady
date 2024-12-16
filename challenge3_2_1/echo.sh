9db6f7e6f1e8d39dc277650fcbd297c2aa569f0c
Abdulaziz Nazirov <nazirovdev@gmail.com>
2024-12-16 16:07:44 +0500

    challenge3_2_1

--- challenge3_2_1/echo.sh
+++ challenge3_2_1/echo.sh
@@ -3,10 +3,10 @@
 # "setVariable" nomli funksiya e'lon qilinmoqda
 setVariable() {
     # Bu funksiya bitta argumentni qabul qiladi
-    # Argumentni "firstName" nomli o‘zgaruvchiga tayinlaydi
-    # O‘zgaruvchi nomi va argumentni ekranga chiqaradi
-    # Masalan: agar argument "marge" bo‘lsa, chiqish "firstName=marge" ko‘rinishida bo‘ladi
-   echo test
+    # Argumentni "firstName" nomli o'zgaruvchiga tayinlaydi
+    # O'zgaruvchi nomi va argumentni ekranga chiqaradi
+    # Masalan: agar argument "marge" bo'lsa, chiqish "firstName=marge" ko'rinishida bo'ladi
+    echo test # Ushbu qator o'chirilishi yoki o'zgarilishi mumkin
 }
 
 # Quyidagi qator "setVariable" funksiyasini "marge" argumenti bilan chaqiradi
