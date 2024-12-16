a4bee3819fa53820c5ca7e7d88fafcc17788abc6
Abdulaziz Nazirov <nazirovdev@gmail.com>
2024-12-16 16:48:17 +0500

    challenge3_2_3

--- challenge3_2_3/addOne.sh
+++ challenge3_2_3/addOne.sh
@@ -4,7 +4,7 @@
 addOne() {
     # Bu funksiya bitta argument qabul qiladi
     # Argumentni bitta oshiradi
-    echo test
+    echo test # Bu qatorni o'chirishingiz yoki o'zgartirishingiz mumkin
 }
 
 # addOne funksiyasini 10 argumenti bilan chaqirish
