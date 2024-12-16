f37c351862e955e45575cb7658f45a4de9c3f3c0
Abdulaziz Nazirov <nazirovdev@gmail.com>
2024-12-16 16:35:59 +0500

    challenge3_2_3

--- challenge3_2_3/addOne_test.sh
+++ challenge3_2_3/addOne_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-source ./addOne.sh
+source /home/challenge3_2_3/challenge3_2_3/addOne.sh
 
 
 testAddOne() {
 	expected=12
 	actual=$(addOne 11)
-	if [ "$actual" == "$expected" ]; then
+	if [ $actual == $expected ]; then
 	    echo "BASH{1_ga_Oshirish}"
 	fi
 
