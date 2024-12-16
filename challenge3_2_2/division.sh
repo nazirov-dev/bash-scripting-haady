175d67a147c7c46d1d6452b1357291e33654595f
Abdulaziz Nazirov <nazirovdev@gmail.com>
2024-12-16 16:34:47 +0500

    challenge3_2_2

--- challenge3_2_2/division.sh
+++ challenge3_2_2/division.sh
@@ -3,8 +3,8 @@
 
 
 divideTwoNum() {
-	# Bu funksiya ikkita argumentni qabul qilad
-	# Natijasini 1-argumentni 2-argumentni bo'lish orqali chiqarini
+	# Bu funksiya ikkita argumentni qabul qiladi
+	# Natijasini 1-argumentni 2-argumentni bo'lish orqali chiqaring
 	# funksiya bo'linadigan haqiqiy musbat musbat sonlarni hisoblaydi
 	# Misol uchun: 1-argumentni 10, 2-argumentni=5 bo'linmasi 2 
 	echo test # bu qismni o'chiring yoki o'zgartiring
