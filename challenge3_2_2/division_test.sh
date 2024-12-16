cc448bd49286cbff717e0a6547f9af8a056f769f
Abdulaziz Nazirov <nazirovdev@gmail.com>
2024-12-16 16:26:52 +0500

    challenge3_2_2

--- challenge3_2_2/division_test.sh
+++ challenge3_2_2/division_test.sh
@@ -6,8 +6,8 @@ source /home/challenge3_2_2/challenge3_2_2/division.sh
 testDivideTwoNum() {
 	expected=2
 	actual=$(divideTwoNum 100 50)
-       if [ "$actual" == "$expected" ]; then
-	 echo "BASH{4_b0l1m_da_yutuq_b0r}"
+       if [ $actual == $expected ]; then
+	 	echo "BASH{4_b0l1m_da_yutuq_b0r}"
        fi	 
 
 
